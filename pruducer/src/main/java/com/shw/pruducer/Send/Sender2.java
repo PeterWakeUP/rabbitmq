bd12000a9d2704a703256ad397ef919e58df9ff9
Peter_shw <787297031@qq.com>
2019-05-09 23:50:58 +0800

    新增callback、returnback

--- pruducer/src/main/java/com/shw/pruducer/Send/Sender2.java
+++ pruducer/src/main/java/com/shw/pruducer/Send/Sender2.java
@@ -8,28 +8,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Sender implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback{
+public class Sender2{
 
     @Autowired
     RabbitTemplate rabbitTemplate;
 
     public void send(Order order)throws Exception{
         CorrelationData correlationData = new CorrelationData(order.getMessageId());
+        //发送消息
         rabbitTemplate.convertAndSend("order-exchange",
                 "order.abcd",
                 order,     //发送内容
                 correlationData);   //发送唯一标识
+
+        //确认模式
+        rabbitTemplate.setConfirmCallback((correlationData1, ack, cause) -> {
+            if (!ack) {
+                //try to resend msg
+            } else {
+                //delete msg in db
+            }
+        });
+
+        //未投递到 queue 退回模式
+        rabbitTemplate.setReturnCallback((Message, i, s1, s2, s3) -> {
+            //TODO
+        });
     }
 
-    //确认模式
-    @Override
-    public void confirm(CorrelationData correlationData, boolean b, String s) {
 
-    }
 
-    //未投递到 queue 退回模式
-    @Override
-    public void returnedMessage(Message message, int i, String s, String s1, String s2) {
 
-    }
+
+
 }
